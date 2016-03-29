df9769a3e3d9a102e7d70e02bafdf01b130ff0ad
wannabeCitizen <doorsofskirpan@gmail.com>
2016-03-28 21:31:51 -0600

    basic set up plus a fade box

---
+++ static/interview.js
@@ -0,0 +1,65 @@
+var user_data;
+
+function preload() {
+    //imgDC = loadImage('static/Yellow-Tree-logo.png');
+    bg_image = loadImage('static/paper2.jpg');
+    folder = loadImage('static/manilla.png');
+    card = loadImage('static/timecard.png');
+    coffee_notes = loadImage('static/cofnotes.png');
+    //Eventually this should use the URL param to make an AJAX call
+    user_data = {
+        name: "Duggy McSwisher",
+        cons: ["said they disliked work 87 times", 
+                "used the word 'late' 150 times",
+                "shares articles about communism",
+                "said drunk 265 times"],
+        pros: ["talked about biking 31 times",
+                "mentioned coffee 129 times",
+                "said they love technology",
+                "shared an article about career achievement"]
+    };
+    handy = loadFont('static/type2.ttf');
+
+}
+
+function setup() {
+    var myCanvas = createCanvas(windowWidth, windowHeight);
+    myCanvas.parent('processing');
+    textFont(handy);
+	fill('rgba(0,0,0,0.75)');
+	noStroke();
+    image(folder, .15*windowWidth, .01*windowHeight);
+    image(bg_image, .2*windowWidth, .2*windowHeight, .5*windowWidth, .8*windowHeight);
+	cardWidth = windowWidth/4.6;
+	cardHeight = card.height/(card.width/(cardWidth));
+	image(card, .02*windowWidth, .25*windowHeight, cardWidth, cardHeight);
+	coffeeWidth = windowWidth/4.85;
+	coffeeHeight = coffee_notes.height/(coffee_notes.width/(coffeeWidth));
+    image(coffee_notes, .7*windowWidth, .09*windowHeight, coffeeWidth, coffeeHeight);
+    textStyle(BOLD);
+    textSize(42);
+    text(user_data.name,.25*windowWidth, .27*windowHeight );
+    textStyle(ITALIC);
+    textSize(36);
+    text("Concerns:", .25*windowWidth, .35*windowHeight);
+    textSize(26);
+    textStyle(NORMAL);
+	var consList = "";
+    for (var i = 0; i < user_data.cons.length; i++){
+        //text("..." + user_data.cons[i], windowWidth*.3, windowHeight*.37 + (windowHeight*.05*i));
+		consList = consList + "..." + user_data.cons[i] + "\n";
+    }
+	text(consList, windowWidth*.3, windowHeight*.37, windowWidth*.5, windowHeight - windowHeight*.37)
+	
+    textSize(32);
+    text("Positive:", .25*windowWidth, .65*windowHeight);
+    textSize(26);
+	var prosList = "";
+    for (var i = 0; i < user_data.pros.length; i++){
+		prosList = prosList + "..." + user_data.pros[i] + "\n";
+    }
+	text(prosList, windowWidth*.3, windowHeight*.67, windowWidth*.5, windowHeight - windowHeight*.65)
+}
+
+function draw() {
+}    
---
+++ static/mirror.js
@@ -0,0 +1,207 @@
+var user_data;
+var static_phrases = {
+    compliments: ["You look beautiful today",
+        "Everyone at work is waiting for you",
+        "You're going to be a star today",
+        "No one is more important than you are",
+        "Love yourself"],
+    subliminal : [
+        "Work harder"],
+    messages : [
+        "wants you to have a great day!",
+        "reminds you to enjoy work!",
+        "hopes you remember they care about you",
+        "winks at your cute self!"],
+    hellos: ["Welcome Back",
+        "Nice to see you",
+        "Happy you're here"]
+
+}
+var live = true;
+//slider
+var compBox;
+//static
+var nameBox;
+//fade
+var msgBox;
+//fade
+var infoBox;
+//slider
+var subBox;
+var hello_text;
+
+function preload() {
+    replay = loadFont('static/BPreplay/BPreplay.otf');
+    skyline = loadFont('static/Small Town Skyline.ttf');
+    user_data = {
+        name: "Michael Skirpan",
+        friends: ["Jackie Cameron",
+        "Julie Cafarella",
+        "Simone Hyater-Adams",
+        "Patrick Cooper"],
+        work: "CU-Boulder",
+
+    }
+    rando = floor(random(0, static_phrases.hellos.length));
+    hello_text = static_phrases.hellos[rando];
+}
+
+function setup() {
+    var myCanvas = createCanvas(windowWidth, windowHeight);
+    myCanvas.parent('processing');
+    textFont(replay);
+    frameRate(10);
+    reBox();
+}
+
+function draw() {
+    background('black');
+    push();
+        fill(255, 255, 255);
+        textFont(replay);
+        textSize(36);
+        textAlign(CENTER);
+        text(hello_text + " " + user_data.name, windowWidth*.5, windowHeight * .2 );
+    pop();
+    msgBox.update();
+
+
+    
+}
+
+var FadeBox = function ( size, x_pos, y_pos, stay, pausing) {
+    this.text_size = size;
+    this.x = x_pos;
+    this.y = y_pos;
+    this.on = false;
+    this.opacity = 0;
+    this.curr_text = '';
+    this.stay = stay;
+    this.going = false;
+    this.fade = 'up';
+    this.phrases = static_phrases['messages'];
+    this.holdoff = pausing;
+    shuffle(this.phrases);
+
+    //Change opacity
+    this.update = function () {
+        if (this.on == false) {
+            this.pause();
+            return;
+        }
+
+        push();
+            textFont(skyline);
+            fill('rgba(255, 255, 255,'+ this.opacity +')');
+            textAlign(CENTER);
+            textSize(this.text_size);
+            text(this.curr_text, this.x, this.y);
+        pop();
+        if (this.going == false) {
+            return;
+        }
+        if (this.fade == 'up') {
+            this.opacity += .05;
+        } else {
+            this.opacity -= .05;
+        }
+        if (this.opacity >= 1) {
+            this.going = false;
+            this.fade = 'down';
+            this.ontime();
+            return;
+        } else if (this.opacity <= 0) {
+            this.on = false;
+            this.going = false;
+            this.opacity = 0;
+        }
+
+    }
+
+    this.ontime = function () {
+        me = this;
+        this.going = false;
+        setTimeout(function () {
+            me.activate();
+        }, me.stay);
+    }
+
+    this.activate = function () {
+        if (this.going == false) {
+            this.going = true;
+        }
+    }
+
+    this.pause = function () {
+        this.on = true;
+        me = this;
+        setTimeout(function () {
+            me.nextText();
+        }, this.holdoff);
+    }
+
+    this.nextText = function () {
+        console.log('next text');
+        rando1 = floor(random(0, user_data['friends'].length));
+        name = user_data['friends'][rando1];
+        if (this.phrases.length > 0) {
+            phrase = this.phrases.pop();
+        } else {
+            stopIt();
+        }
+        this.curr_text = name + " " + phrase;
+        this.on = true;
+        this.going = true;
+        this.opacity = 0;
+        this.fade = 'up';
+    }
+}
+
+var SlideBox = function ( size, y_pos, orient, font, speed ) {
+    //orient == left -> textAlign(RIGHT)
+    this.orient = orient;
+    this.font = font;
+    this.y_pos = y_pos;
+    this.text_size = size;
+    this.on = false;
+    this.speed = speed;
+
+}
+
+//slider
+// var compBox;
+// //static
+// var nameBox;
+// //fade
+// var msgBox;
+// //fade
+// var infoBox;
+// //slider
+// var subBox;
+
+function reBox() {
+    //Take new user data and fill boxes
+    msgBox = new FadeBox(48, windowWidth*.5, windowHeight*.8, 3000, 2000);
+
+}
+
+function stopIt() {
+    //Call when user scans again
+    noLoop();
+    background(0, 0 ,0);
+    live = false;
+}
+
+function startIt() {
+    //Call after a new scanner call
+    loop();
+    reBox();
+}
+
+function mousePressed() {
+    if (live == true) {
+        stopIt();
+    } else {
+        startIt();
+    }
+}
